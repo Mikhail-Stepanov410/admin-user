74331a6461c007a149055d8218d03ed45f5e7613
Mikhail-Stepanov410 <m_stepanov@Terminal.local>
2025-06-23 11:07:19 +0500

    Minor code updates and improvements

--- login.php
+++ login.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
 
-  <!-- Подключение внешнего стиля -->
+
   <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
